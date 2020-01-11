75c3d1583bd2621890f2f4d78864c213292363b8
TiagoSeverino <tiago@severino.xyz>
2020-01-11 16:45:15 +0000

    Create Organization Table

---
+++ console/migrations/m200111_163313_Add_Organizations.php
@@ -0,0 +1,58 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200111_163313_Add_Organizations
+ */
+class m200111_163313_Add_Organizations extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute("
+            CREATE TABLE Organization
+            (
+                ID INT NOT NULL AUTO_INCREMENT,
+                Name VARCHAR(64) NOT NULL,
+                CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (ID)
+            );
+
+            CREATE TABLE OrganizationMember
+            (
+                OrganizationID INT NOT NULL UNIQUE,
+                UserID INT NOT NULL UNIQUE,
+                CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (UserID) REFERENCES User (ID) ON DELETE CASCADE,
+                FOREIGN KEY (OrganizationID) REFERENCES Organization (ID) ON DELETE CASCADE
+            );
+        ");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->execute("DROP TABLE IF EXISTS OrganizationMember;
+                        DROP TABLE IF EXISTS Organization;");
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200111_163313_Add_Organizations cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
