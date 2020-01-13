06fcb5b4dc4b747e97f2563ca7c72ecd2fdbaab2
TiagoSeverino <tiago@severino.xyz>
2020-01-13 16:46:24 +0000

    Add Organization CRUD Frontend

--- common/models/User.php
+++ common/models/User.php
@@ -209,4 +209,12 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $this->password_reset_token = null;
     }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrganizations()
+    {
+        return $this->hasMany(Organization::className(), ['id' => 'OrganizationID'])->viaTable('organizationmember', ['UserID' => 'id']);
+    }
 }
---
+++ frontend/views/organization/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Organization */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="organization-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'Name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'CreatedAt')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
