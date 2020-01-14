e8ab5d8b3294e9e1f0f6ac1999d58c6b51c38947
TiagoSeverino <tiago@severino.xyz>
2020-01-14 16:00:51 +0000

    Add Room Controller

---
+++ frontend/views/Room/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Room */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="room-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'OrganizationID')->textInput() ?>
+
+    <?= $form->field($model, 'Name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Chairs')->textInput() ?>
+
+    <!-- <?= $form->field($model, 'CreatedAt')->textInput() ?> -->
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -38,6 +38,7 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'Organization', 'url' => ['/organization']],
+        ['label' => 'Room', 'url' => ['/room']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
