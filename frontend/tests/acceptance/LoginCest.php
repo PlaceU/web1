f3c4d777e5e7db532719b7c29e32c50bd2802943
TiagoSeverino <tiago@severino.xyz>
2020-01-17 16:27:28 +0000

    Create acceptance test

---
+++ frontend/tests/acceptance/LoginCest.php
@@ -0,0 +1,20 @@
+<?php
+namespace frontend\tests\acceptance;
+
+use frontend\tests\AcceptanceTester;
+use yii\helpers\Url;
+
+class LoginCest
+{
+    public function checkLogin(AcceptanceTester $I)
+    {
+        $I->amOnPage(Url::toRoute('/site/login'));
+        $I->seeInTitle('Login');
+
+        $I->seeLink('Signup');
+        $I->click('Signup');
+        $I->wait(3); // wait for page to be opened
+
+        $I->see('Please fill out the following fields to signup:');
+    }
+}
