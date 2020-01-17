a9e9dd60fbcfe82b75a9d2dd74126d20a5fa08a9
TiagoSeverino <tiago@severino.xyz>
2020-01-17 17:14:38 +0000

    Create OrganizationCest

---
+++ frontend/tests/acceptance/OrganizationCest.php
@@ -0,0 +1,41 @@
+<?php
+namespace frontend\tests\acceptance;
+
+use common\fixtures\UserFixture;
+use frontend\tests\AcceptanceTester;
+use yii\helpers\Url;
+
+class OrganizationCest
+{
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php',
+            ],
+        ];
+    }
+
+    public function testCreateOrganization(AcceptanceTester $I, LoginCest $login)
+    {
+        $login->testLogin($I);
+        
+        $I->amOnPage(Url::toRoute('/organization'));
+
+        $I->click('Create Organization');
+
+        $I->wait(2);
+
+        $I->fillField('Organization[Name]', 'Lab Center');
+        $I->see('Save');
+        
+        $I->click('Save');
+
+
+        $I->wait(2);
+
+        $I->see('Update');
+        $I->see('Delete');
+    }
+}
