d0a9295c11ed4d8897624784f65bfbcc161cb7dc
TiagoSeverino <tiago@severino.xyz>
2020-01-17 17:14:24 +0000

    Add testLogin

--- frontend/tests/acceptance/LoginCest.php
+++ frontend/tests/acceptance/LoginCest.php
@@ -1,12 +1,30 @@
 <?php
 namespace frontend\tests\acceptance;
 
+use common\fixtures\UserFixture;
 use frontend\tests\AcceptanceTester;
 use yii\helpers\Url;
 
 class LoginCest
 {
-    public function checkLogin(AcceptanceTester $I)
+    /**
+     * Load fixtures before db transaction begin
+     * Called in _before()
+     * @see \Codeception\Module\Yii2::_before()
+     * @see \Codeception\Module\Yii2::loadFixtures()
+     * @return array
+     */
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php',
+            ],
+        ];
+    }
+
+    public function checkLoginPage(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/login'));
         $I->seeInTitle('Login');
@@ -16,5 +34,20 @@ class LoginCest
         $I->wait(2); // wait for page to be opened
 
         $I->see('Please fill out the following fields to signup:');
+    }
+
+    public function testLogin(AcceptanceTester $I)
+    {
+        $I->amOnPage(Url::toRoute('/site/login'));
+        $I->seeInTitle('Login');
+
+        $I->submitForm('#login-form', array(
+            'LoginForm[username]' => 'erau',
+            'LoginForm[password]' => 'password_0'
+        ));
+
+        $I->wait(2);
+
+        $I->see('Organization');
     }
 }
